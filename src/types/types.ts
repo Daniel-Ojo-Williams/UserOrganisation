3e8a20bb37e9f16e152bf0d3ac505a1ed0aee09e
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-07-07 00:10:10 +0100

    patch: extend express' request to expose user object

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,7 +2,15 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv extends ENVCONFIG {}
   }
+
+  namespace Express {
+    export interface Request {
+      user?: User; 
+    }
+  }
 }
+
+export type User = { userId: string, email: string }
 
 export interface ENV {
   JWT_SECRET?: string;
