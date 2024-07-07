c75dc1280bda6c89c7ac043580664ce7b4fb8442
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-07-07 16:17:30 +0100

    patch: separate server listening from express app, for testing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import { config } from './config';
 import { OrgRoutes, UserRoutes } from './routes';
 
 const app = express();
@@ -13,13 +12,4 @@ app.use(helmet());
 app.use('/auth', UserRoutes);
 app.use('/api', OrgRoutes);
 
-
-const PORT = config.PORT;
-
-app.listen(PORT, () => {
-  console.log(`App running on ${PORT}`)
-});
-
-
-
-
+export default app;
