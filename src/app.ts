6c5f1627c1051e92fd210c6cd40986160a824748
Ojo-williams Daniel <freakydmuse@gmail.com>
2024-07-07 17:22:04 +0100

    patch: welcome on home and response on invalid endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import { OrgRoutes, UserRoutes } from './routes';
+import { HttpCode } from './utils';
 
 const app = express();
 app.use(express.json());
@@ -11,5 +12,13 @@ app.use(helmet());
 
 app.use('/auth', UserRoutes);
 app.use('/api', OrgRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(HttpCode.OK).json({ message: 'Welcome to stage 2 server' })
+});
+
+app.all('*', (req: Request, res: Response) => {
+  res.status(HttpCode.NOT_FOUND).json({ message: 'Invalid endpoint or method used, please check and try again' })
+})
 
 export default app;
